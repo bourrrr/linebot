69fe599e6697b86d10540b09afcc0b73ed27c132
unknown <C111193105@nkust.edu.tw>
2025-07-09 19:09:56 +0800

    修正 msg 未定義錯誤，完成訊息處理區塊包裝

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ const saveImage = require("./OCR_modules/saveImage"); // 儲存圖片
 const runOCR = require("./OCR_modules/ocr"); 
 const healthCard = require("./OCR_modules/flex/healthCard.js"); 
 console.log('📦 saveImage 模組載入成功');
-const healthflex = require('./OCR_modules/flex/healthFlex');
+const healthflex = require('./OCR_modules/healthFlex');
 // 初始化 Firebase
 if (!admin.apps.length) {
   admin.initializeApp({
