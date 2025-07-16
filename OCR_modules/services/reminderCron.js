68cddffdff88107d3feb5d97be56899f1be1b6b1
unknown <C111193105@nkust.edu.tw>
2025-07-17 00:05:55 +0800

    更新 Flex Message 連結

--- OCR_modules/services/reminderCron.js
+++ OCR_modules/services/reminderCron.js
@@ -1,5 +1,3 @@
-// OCR_modules/services/reminderCron.js
-
 const cron = require('node-cron');
 const admin = require('firebase-admin');
 
@@ -9,6 +7,9 @@ function startReminderCron(db, client) {
     const now = new Date();
     const minBefore = new Date(now.getTime() - 60000);
     const minAfter = new Date(now.getTime() + 60000);
+
+    // 每分鐘都印一次 cron 是否有在跑
+    console.log('[cron] 任務執行，現在時間:', now.toISOString());
 
     try {
       const usersSnapshot = await db.collection('users').get();
@@ -21,9 +22,24 @@ function startReminderCron(db, client) {
           .where('datetime', '<=', admin.firestore.Timestamp.fromDate(minAfter))
           .get();
 
+        // 加一行 log 看這個 user 是否有 reminder 資料
+        console.log(`[cron] userId: ${userId} snapshot.size: ${snapshot.size}`);
+
+        if (snapshot.size === 0) {
+          // 沒有資料的情況也印出來方便 debug
+          console.log(`[cron] userId: ${userId} 這分鐘沒有需要推播的提醒`);
+        }
+
         snapshot.forEach(async (doc) => {
           const data = doc.data();
-		console.log('推播用藥提醒:', userId, data.medicine, doc.id, data.datetime && data.datetime.toDate && data.datetime.toDate());
+          // 推播前詳細 log
+          console.log('[cron] 推播用藥提醒:', {
+            userId,
+            medicine: data.medicine,
+            docId: doc.id,
+            datetime: data.datetime && data.datetime.toDate && data.datetime.toDate()
+          });
+
           // 發送提醒訊息
           await client.pushMessage(userId, {
             type: 'template',
