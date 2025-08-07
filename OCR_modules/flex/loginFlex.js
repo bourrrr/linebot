e5e38490c3636a73a583d5a57965698616deba37
unknown <C111193105@nkust.edu.tw>
2025-08-07 22:31:25 +0800

    更新 Flex Message 連結

--- OCR_modules/flex/loginFlex.js
+++ OCR_modules/flex/loginFlex.js
@@ -57,7 +57,7 @@ function loginFlex() {
             action: {
               type: "uri",
               label: "我是志工",
-              uri: "https://medwell-test1.web.app/45678/login.html?role=volunteer"
+              uri: "https://medwell-test1.web.app/45678/login.html?role=volunteerBtn"
             }
           }
         ]
