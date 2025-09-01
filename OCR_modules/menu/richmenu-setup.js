95d007928b5349edc79555df93b3d7ac9e8ed91a
unknown <C111193105@nkust.edu.tw>
2025-09-02 00:24:30 +0800

    更新 Flex Message 連結

--- OCR_modules/menu/richmenu-setup.js
+++ OCR_modules/menu/richmenu-setup.js
@@ -20,7 +20,7 @@ async function setupRichMenus() {
 
   const size = { width: 2500, height: 1686 };
 
-  // ====== A. 健康照護 分頁 ======
+  // ====== A. 社區服務 分頁 ======
   const careRichMenu = await client.createRichMenu({
  
     areas: [
@@ -29,11 +29,11 @@ async function setupRichMenus() {
         bounds: { x: 1250, y: 0, width: 1250, height: 220 },
         action: { type: 'richmenuswitch', richMenuAliasId: 'alias-service', data: 'to-service' }
       },
-      { bounds: { x: 1340, y: 260, width: 520, height: 380 },
+      { bounds: { x: 1069, y: 450, width: 680, height: 450 },
         action: { type: 'message', text: '藥局地圖' } },
-      { bounds: { x: 1900, y: 260, width: 520, height: 380 },
+      { bounds: { x: 1794, y: 450, width: 680, height: 450 },
         action: { type: 'message', text: '飲食推薦' } },
-      { bounds: { x: 80, y: 1200, width: 1200, height: 480 },
+      { bounds: { x: 1125, y: 1013, width: 1280, height: 460 },
         action: { type: 'message', text: '志工配對' } }
     ]
   });
@@ -44,7 +44,7 @@ async function setupRichMenus() {
   );
   await client.createRichMenuAlias(careRichMenu, 'alias-care');
 
-  // ====== B. 社區服務 分頁 ======
+  // ====== B.健康照護 分頁 ======
   const serviceRichMenu = await client.createRichMenu({
 
     areas: [
@@ -53,13 +53,13 @@ async function setupRichMenus() {
         action: { type: 'richmenuswitch', richMenuAliasId: 'alias-care', data: 'to-care' }
       },
 
-      { bounds: { x: 70, y: 450, width: 525, height: 380 },
-        action: { type: 'message', text: '幫助' } },
-      { bounds: { x: 654, y: 450, width: 520, height: 380 },
+      { bounds: { x: 71, y: 440, width: 640, height: 450 },
+        action: { type: 'message', text: '功能說明' } },
+      { bounds: { x: 786, y:441, width: 640, height: 450 },
         action: { type: 'message', text: '健康數據紀錄' } },
-      { bounds: { x: 70, y: 930, width: 520, height: 380 },
+      { bounds: { x: 71, y: 1045, width: 640, height: 450 },
         action: { type: 'message', text: '用藥提醒' } },
-      { bounds: { x: 654, y: 930, width: 525, height: 380 },
+      { bounds: { x: 786, y: 1045, width: 640, height: 450 },
         action: { type: 'uri', uri: 'https://medwell-test1.web.app/newcard/indexcard.html' } }
     ]
   });
