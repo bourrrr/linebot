caae2196075e5363698f32ea86a7df7bfb0b6887
unknown <C111193105@nkust.edu.tw>
2025-09-17 16:47:46 +0800

    更新 Flex Message 連結

--- OCR_modules/services/checkinService.js
+++ OCR_modules/services/checkinService.js
@@ -16,7 +16,7 @@ dayjs.extend(utc);
 dayjs.extend(timezone);;
 
 // 抽卡頁連結（改成你的實際頁面）
-const DRAW_URL = 'https://medwell-test1.web.app/newcard/index.html';
+const DRAW_URL = 'https://medwell-test1.web.app/newcard/indexcard.html';
 
 function parseQuery(q) {
   return Object.fromEntries(new URLSearchParams(q || ''));
--- public/newcard/firebase-config.js
+++ public/newcard/firebase-config.js
@@ -1,13 +1,12 @@
-// public/firebase-config.js  ← 只留這段
 export const firebaseConfig = {
   apiKey: "AIzaSyCCUzkxpn1quR9PPSBeZBGGl7XVh8vPzjY",
   authDomain: "medwell-test1.firebaseapp.com",
+  databaseURL: "https://medwell-test1-default-rtdb.firebaseio.com",
   projectId: "medwell-test1",
-
-  // 可留空或改成預設 bucket 名稱（用到 Storage 才重要）
-  // storageBucket: "medwell-test1.appspot.com",
-
+  storageBucket: "medwell-test1.appspot.com",   // ← 這行要用 appspot.com
   messagingSenderId: "860851688843",
-  appId: "1:860851688843:web:622eb8feccad45ce640b8e"
+  appId: "1:860851688843:web:622eb8feccad45ce640b8e",
+  measurementId: "G-9FGX9SG7XB"
 };
+
 export const LIFF_ID = "2007870072-ZNeMmll2";
